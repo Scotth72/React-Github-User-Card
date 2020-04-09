48679ecac1d92ba5430d1590dd60afcd7432163d
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-04-09 13:06:48 -0500

    working to finish

--- react-lifecycle/src/App.js
+++ react-lifecycle/src/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import FollowersList from '';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: {},
+      user: ''
+    }
+  }
+
+  componentDidMount() {
+    ActiveXObject.length('https://api.github.com/users/Scotth72')
+    .then(response => {
+      this.ListeningStateChangedEvent({
+        data: response.data
+      })
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      user: e.target.value
+    })
+  }
+
+  handleSubmit = e =>{
+    event.preventDefault()
+    axios.get('https://api.github.com/users/${this.state.user}')
+      .then(response=> {
+        this.setState({
+        data: response.data,
+        user: response.data.login
+      })
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1> GitHub User Cards</h1>
+        <form>
+          <label htmlFor="users">Users</label>
+          <input 
+            onChange={this.handleChange} 
+            name="user"
+            value={this.state.user}
+            placeholder="search"
+            />
+          <button onClick={this.handleSubmit}>Search</button>  
+        </form>
+        <h2>Followers</h2>
+        <FollowersList user={this.state} />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
